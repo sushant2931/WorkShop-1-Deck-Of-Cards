3c3e8d6a270558ca534f3a83722aae3c38747bb4
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 18:38:54 +0530

    Added:SetUp the game With Deck of Cards

--- src/com/bridgelabz/deckofcard/DeckCard.java
+++ src/com/bridgelabz/deckofcard/DeckCard.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.deckofcard;
 
 public class DeckCard {
-	public static void main(String[] args) {
-		System.out.println("Welcome On Deck Of Card Program");
+	
+		 static String[] Clubs = { "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9","C10" , "CJack", "CQueen", "CKing", "CAce" };
+		 static String[] Diamonds = {"D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9","D10",  "DJack", "DQueen", "DKing", "DAce" };
+	     static String[] Hearts = { "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9","H10", "HJack", "HQueen", "HKing", "HAce" };
+		 static String[] Spades = { "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJack", "SQueen", "SKing", "SAce" };
+		 static String[][] deck = {Clubs,Diamonds, Hearts, Spades}; 
+		
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to the DeckCards computation program");
+			showAllCards();
+			
+		}
+		private static void showAllCards() {
+			System.out.println("Displaying total cards");
+			for(int i=0;i<4;i++) {
+				for(int j=0;j<13;j++) {
+					System.out.println(" "+deck[i][j]);
+		}
+				System.out.println();
+		}
 	}
 }
+
